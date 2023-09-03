c101bb902b8b0ceeaecd83ce2c7a0099614b96cd
Quan Nguyen <vozermuonnam1@gmail.com>
2023-09-03 18:44:12 +0700

    ability of javascript

--- video3/video3.js
+++ video3/video3.js
@@ -1,3 +1,20 @@
+
+function onShow() {
+   let contentElement = window.document.getElementById("content");
+   contentElement.style.display = 'block';
+}
+
+function onHide() {
+   let currentElement = window.document.getElementById("content");
+   currentElement.style.display = 'none';
+}
+function onChangeSize() {
+   console.log("Goo here");
+   let currentElement = window.document.getElementById("demo");
+   currentElement.style.fontSize = '50px';
+}
+
+
 function turnOn() {
 
    let imageElement = document.getElementById("light");
