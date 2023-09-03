2cf824fd7c07d10589f5c06ace33671786640ed2
Quan Nguyen <vozermuonnam1@gmail.com>
2023-09-03 11:38:30 +0700

    temp

--- video4/video4.js
+++ video4/video4.js
@@ -2,5 +2,19 @@ let a = `I'm a string `;
 let text = `We are the so-called 
 "Vikings" from the north.`;
 
+let x = 16 + "abc";
+
+console.log(x);
+
+let y = 12 + 1 + "abc";
+
+console.log(y);
+
+let x1 = "abc" + 16 + 1;
+console.log(x1);
+
+// Date
+console.log(new Date());
+
 console.log(text)
 console.log(a)
