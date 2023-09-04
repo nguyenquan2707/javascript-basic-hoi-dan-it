ca0cd61d6cad0add359e640291a059ab8a85402e
Quan Nguyen <vozermuonnam1@gmail.com>
2023-09-04 11:18:37 +0700

    some of methods and property of location of document of windown object

--- video3/video3.js
+++ video3/video3.js
@@ -1,6 +1,7 @@
+window.document.getElementById("demo").innerHTML = "href=" + window.document.location.href
++ ", HostName=" + window.document.location.hostname + ", pathName=" + window.document.location.pathname
++ ", protocol=" + window.document.location.protocol + ", port=" + window.document.location.port;
 
-// some of property of screen of windown.
-document.getElementById("demo").innerHTML = window.screen.availHeight + " " + window.screen.availWidth;
-document.getElementById("demo2").innerHTML = "height= " + window.screen.height + ", width= " + window.screen.width;
-document.getElementById("demo3").innerHTML = "Depth Color = " + window.screen.colorDepth + ", pixel depth=" + window.screen.pixelDepth;
- 
+function openNewDocument() {
+   window.document.location.assign("https://www.w3schools.com");
+}
