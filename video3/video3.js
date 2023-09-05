02c52d3d6d7b449303f062bff9d46d93c8119493
Quan Nguyen <vozermuonnam1@gmail.com>
2023-09-05 21:13:51 +0700

    only number

--- video3/video3.js
+++ video3/video3.js
@@ -1,31 +1,3 @@
-
-function onShow() {
-   let contentElement = window.document.getElementById("content");
-   contentElement.style.display = 'block';
-}
-
-function onHide() {
-   let currentElement = window.document.getElementById("content");
-   currentElement.style.display = 'none';
-}
-function onChangeSize() {
-   console.log("Goo here");
-   let currentElement = window.document.getElementById("demo");
-   currentElement.style.fontSize = '50px';
-}
-
-
-function turnOn() {
-
-   let imageElement = document.getElementById("light");
-   imageElement.src = "./assets/on.gif";
-}
-
-function turnOff() {
-
-    let imageElement = document.getElementById("light");
-    imageElement.src = "./assets/off.gif";
- }
- 
-
-
+let a = 1;
+let b = 2;
+console.log(a + b);
