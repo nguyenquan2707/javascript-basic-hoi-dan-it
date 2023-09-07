1b4d44cc0ee1ef598caf2dc58fdf801521ab1cbb
Quan Nguyen <vozermuonnam1@gmail.com>
2023-09-07 21:03:47 +0700

    object

--- video3/video3.js
+++ video3/video3.js
@@ -1,6 +1,23 @@
 console.log(`A bc `);
 
-let a = 1;
-let b = 2;
+let a = {}
 
-console.log(`Rusult from a +b = ${a + b} .`);
+//Khai báo gồm key và value
+//Key ko nên có dấu cách
+
+let person = {
+    name: "Quan",
+    address: "Hcm",
+    methodTest: () => {
+        console.log("Hi...");
+        return `Hi`;
+    }
+}
+
+console.log("type of a = " , typeof a);
+console.log("name =  " , person.name);
+// https://www.youtube.com/watch?v=xEBkBJDgAY4&list=PLncHg6Kn2JT5dfQqpVtfNYvv3EBVHHVKo&index=12
+// Cách này lợi thế hơn
+console.log("Hoặc truy cập name bằng " , person['name']);
+const test = person.methodTest();
+console.log(test);
