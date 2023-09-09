1880d9365430a6b35c0f8bb64065ed1931036f53
Quan Nguyen <vozermuonnam1@gmail.com>
2023-09-09 10:06:52 +0700

    for loops

--- video3/video3.js
+++ video3/video3.js
@@ -1,29 +1,5 @@
 console.log(`A bc `);
 
-let a = {
-    name: "Q",
-    age: 35
+for(let i = 0; i < 10; i ++) {
+    console.log('i = ', i);
 }
-
-console.log(a);
-
-let b = ["A", "B", "C"];
-
-console.log(`type of b = `, typeof b, b); //https://ibb.co/D8BrtT3
-
-// https://ibb.co/9y4QRmD
-
-console.log(b[0]);
-
-let customers = [
-    {
-        name: "A",
-        age: 23
-    },
-    {
-        name: "B",
-        age: 22
-    }
-];
-
-console.log(customers);
