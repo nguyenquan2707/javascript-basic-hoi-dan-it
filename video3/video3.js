258a5f7b9b39125fdff1d32c5b04c0ffd7381a84
Quan Nguyen <vozermuonnam1@gmail.com>
2023-09-10 13:39:42 +0700

    async await

--- video3/video3.js
+++ video3/video3.js
@@ -1,16 +1,14 @@
-console.log(`A bc `);
-//đồng bộ là chạy thứ tự từng dòng từ trên xuống dưới.
+// Hàm có async này trở thành promise
+const getNewToDo = async() => {
 
-//Async là xữ lý không cần thứ tự, có dòng done trước, có dòng done sau.
-//Nhưng vẩn đảm bảo là đầu ra chính xác.
-
-//https://jsonplaceholder.typicode.com/todos/1
-
-console.log(1);
-console.log(2);
-//sleep
-setTimeout(() => {
-    console.log(`run after 3s`);
     
-}, 3000); //3s
-console.log(4);
+    //promis pending nghĩa là code đã xong rồi, mà promis vẩn chưa trả ra kết quả.như vậy gọi là pending,
+    //bởi vì javascript là bất đồng bộ, nếu mình không nói với nó là mày phải chờ tao, thì để nói với javascript 
+    //rằng mày phải chờ tao, thì js có tứ await, nghĩa là chờ tao nha.
+    // let data = fetch('https://jsonplaceholder.typicode.com/todos/1') ////https://ibb.co/V2JqWpK
+    let response = await fetch('https://jsonplaceholder.typicode.com/todos/1')//chờ tao lấy data
+    console.log(`Data = `, response);
+    console.log(`Data json = `, await response.json()); //Bởi vì json() cũng trả ra promis nên phải thêm await
+}
+// Promise, code này chờ để chạy, mình phải làm gì để cho promise này không còn promise nữa.
+console.log(getNewToDo());
