5d285bea6834371f0d8ef224097a300e50209208
Quan Nguyen <vozermuonnam1@gmail.com>
2023-09-10 15:58:42 +0700

    .

--- video3/video3.js
+++ video3/video3.js
@@ -1,5 +1,9 @@
 console.log(`A bc `);
+let array = [
+    "A", "B", "C", "D"
+];
 
-for(let i = 0; i < 10; i ++) {
-    console.log('i = ', i);
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log(element);
 }
