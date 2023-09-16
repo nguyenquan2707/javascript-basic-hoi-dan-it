7014ca6a93408d0666281535dbbf1017874f8d16
Quan Nguyen <vozermuonnam1@gmail.com>
2023-09-16 10:51:32 +0700

    mapArrays

--- video3/video3.js
+++ video3/video3.js
@@ -1,56 +1,13 @@
-console.log(`A bc `);
-
-let a = {
-    name: "Q",
-    age: 35
-}
-
-console.log(a);
-
-let b = ["A", "B", "C"];
-
-console.log(`type of b = `, typeof b, b); //https://ibb.co/D8BrtT3
-
-// https://ibb.co/9y4QRmD
-
-console.log(b[0]);
-
-let customers = [
-    {
-        name: "A",
-        age: 23
-    },
-    {
-        name: "B",
-        age: 22
-    }
-];
-
-console.log(customers.length);
+let myArray = [1, 2, 3, 4, 5];
 
 
-b.push("D");
-console.log(`b=`, b);
-
-// 
-let words = ['Quan', 'Tra', 'An', 'A', 'Nguyen'];
-let results = words.filter((element, index) => {
-    console.log(`${index} : ${element}`);
-    return index % 2 == 0;
+//Create new map
+let mapArray = myArray.map((value, index) => {
+    return value * 2;
+});
+console.log(mapArray);
+let mapArray2 = myArray.map((value, index, array) => {
+    return value * 2;
 });
 
-console.log(results);
-
-// 
-let employees = [
-    {name: "Q", age: 35},
-    {name: "T", age: 34},
-    {name: "A", age: 20},
-    {name: "AA", age: 25},
-];
-
-let filterdEmployees = employees.filter((element, index) => {
-    return element.age > 25;
-});
-
-console.log(filterdEmployees);
+console.log(mapArray2);
