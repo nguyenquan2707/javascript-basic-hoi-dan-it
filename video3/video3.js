25e2a10d8894ae76477a122fe0a111c825dbef47
Quan Nguyen <vozermuonnam1@gmail.com>
2023-09-17 10:47:24 +0700

    reduce

--- video3/video3.js
+++ video3/video3.js
@@ -1,56 +1,9 @@
-console.log(`A bc `);
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+const array1 = [1, 2, 3, 4];
 
-let a = {
-    name: "Q",
-    age: 35
-}
+// 0 + 1 + 2 + 3 + 4
+const initialValue = 0;
+const sumWithInitial = array1.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue);
 
-console.log(a);
-
-let b = ["A", "B", "C"];
-
-console.log(`type of b = `, typeof b, b); //https://ibb.co/D8BrtT3
-
-// https://ibb.co/9y4QRmD
-
-console.log(b[0]);
-
-let customers = [
-    {
-        name: "A",
-        age: 23
-    },
-    {
-        name: "B",
-        age: 22
-    }
-];
-
-console.log(customers.length);
-
-
-b.push("D");
-console.log(`b=`, b);
-
-// 
-let words = ['Quan', 'Tra', 'An', 'A', 'Nguyen'];
-let results = words.filter((element, index) => {
-    console.log(`${index} : ${element}`);
-    return index % 2 == 0;
-});
-
-console.log(results);
-
-// 
-let employees = [
-    {name: "Q", age: 35},
-    {name: "T", age: 34},
-    {name: "A", age: 20},
-    {name: "AA", age: 25},
-];
-
-let filterdEmployees = employees.filter((element, index) => {
-    return element.age > 25;
-});
-
-console.log(filterdEmployees);
+console.log(`Reduce...` , sumWithInitial);
+// Expected output: 10
