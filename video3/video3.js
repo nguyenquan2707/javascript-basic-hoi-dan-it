973a8dbd48767bacc2888983fd34bf90a290e105
Quan Nguyen <vozermuonnam1@gmail.com>
2023-09-17 13:08:44 +0700

    sort array

--- video3/video3.js
+++ video3/video3.js
@@ -1,9 +1,30 @@
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
-const array1 = [1, 2, 3, 4];
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
 
-// 0 + 1 + 2 + 3 + 4
-const initialValue = 0;
-const sumWithInitial = array1.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue);
+const array1 = [1, 30, 4, 21, 100000];
+array1.sort((first, second) => {
+    return first - second;
+});
 
-console.log(`Reduce...` , sumWithInitial);
-// Expected output: 10
+console.log(array1);
+
+const months = ['March', 'Jan', 'Feb', 'December'];
+
+months.sort((firstElement, secondElement) => {
+    return firstElement.length - secondElement.length;
+});
+console.log(months);
+
+
+const items = [
+    { name: "Edward", value: 21 },
+    { name: "Sharpe", value: 37 },
+    { name: "And", value: 45 },
+    { name: "The", value: -12 },
+    { name: "Magnetic", value: 13 },
+    { name: "Zeros", value: 37 },
+  ];
+  
+  // sort by value
+  items.sort((a, b) => a.value - b.value);
+
+  console.log(items);
