c21ef31b305fc1393134ce234011485a09811706
Quan Nguyen <vozermuonnam1@gmail.com>
2023-09-17 20:22:44 +0700

    chunk

--- video3/video3.js
+++ video3/video3.js
@@ -5,3 +5,10 @@ var listOfNumbers = '';
 _.each(numbers, function(x) { listOfNumbers += x + ' ' });
 
 console.log(listOfNumbers);
+
+// chunk
+var numbers2 = [1, 2, 3, 4];
+var listOfNumbers2 = '';
+
+listOfNumbers2 = _.chunk(numbers2, 2);
+console.log(listOfNumbers2);
