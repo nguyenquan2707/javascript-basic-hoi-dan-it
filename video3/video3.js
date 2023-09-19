700ddc372333b733436f3ed57f80d76bcee4abec
Quan Nguyen <vozermuonnam1@gmail.com>
2023-09-19 22:14:24 +0700

    promise 2

--- video3/video3.js
+++ video3/video3.js
@@ -1,8 +1,8 @@
 const promisesTest = () => {
 
     return new Promise((resolve, reject) => {
-        // resolve(`Hi....`);
-        reject('Network low.....')
+        resolve(`Hi....`);
+        // reject('Network low.....')
     });
 };
 
@@ -14,3 +14,33 @@ promisesTest().then((data) => {
 }).catch((reject) => {
     console.log(reject);
 })
+
+function testPromise() {
+
+    return new Promise((resolve, reject) => {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+              //JSON test
+            // document.getElementById("demo").innerHTML = JSON.parse(this.responseText).title;
+
+            const data = JSON.parse(xhttp.responseText);
+            resolve(data);
+
+          } else if (this.readyState == 4 && this.status != 200){
+            reject(`Something wrong....`);
+              console.log(xhttp.status);
+          }
+        };
+
+        xhttp.open("GET", "https://jsonplaceholder.typicode.com/todos/1", true);
+        xhttp.send();
+    });
+  }
+
+  testPromise().then((data) => {
+    console.log(data)
+  })
+  .catch((reject) => {
+console.log(reject)
+  })
