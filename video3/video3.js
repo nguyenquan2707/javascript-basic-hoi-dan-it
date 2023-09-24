5b50f85e5d6538f4f1a317c8ebecd6799e32ddb9
Quan Nguyen <vozermuonnam1@gmail.com>
2023-09-24 18:49:09 +0700

    errors

--- video3/video3.js
+++ video3/video3.js
@@ -1,14 +1,33 @@
-// Hàm có async này trở thành promise
-const getNewToDo = async() => {
+/*const getNewToDo = async() => {
+    let response = await fetch('https://jsonplaceholder.typicode.com/todos/1ssss')
+    // console.log(`>>> check response = `, response);
+    if(response && response.status !== 200) {
+        // Lưu ý: 
+        //nếu return thì nó vào then return new Error(`Something wrong with status = ` + response.status);
+        //dùng throw thì nó vào catch.
+        throw new Error(`Something wrong with status = ` + response.status);
+    }
+    let data = await response.json();
+    return data;
+}*/
 
-    
-    //promis pending nghĩa là code đã xong rồi, mà promis vẩn chưa trả ra kết quả.như vậy gọi là pending,
-    //bởi vì javascript là bất đồng bộ, nếu mình không nói với nó là mày phải chờ tao, thì để nói với javascript 
-    //rằng mày phải chờ tao, thì js có tứ await, nghĩa là chờ tao nha.
-    // let data = fetch('https://jsonplaceholder.typicode.com/todos/1') ////https://ibb.co/V2JqWpK
-    let response = await fetch('https://jsonplaceholder.typicode.com/todos/1')//chờ tao lấy data
-    console.log(`Data = `, response);
-    console.log(`Data json = `, await response.json()); //Bởi vì json() cũng trả ra promis nên phải thêm await
+/** Try catch way */
+
+const getNewToDo = async() => {
+    try {
+        let response = await fetch('https://jsonplaceholder.typicode.com/todos/1ssss')
+        if(response && response.status !== 200) {
+            throw new Error(`Something wrong with status = ` + response.status);
+        }
+        let data = await response.json();
+        return data;
+    } catch(error) {
+        console.log(`error====`,error.message);
+    }
 }
-// Promise, code này chờ để chạy, mình phải làm gì để cho promise này không còn promise nữa.
-console.log(getNewToDo());
+
+getNewToDo().then((data) => {
+    console.log(`data = `, data);
+}).catch((reject) => {
+    console.log(`reject =` + reject.message);
+})
