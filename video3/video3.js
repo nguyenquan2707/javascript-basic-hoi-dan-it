8214379482f97099a02924ccb5512aaafdbdf2b2
Quan Nguyen <vozermuonnam1@gmail.com>
2023-09-26 21:19:57 +0700

    index

--- video3/video3.js
+++ video3/video3.js
@@ -3,6 +3,7 @@ let myArray = [1, 2, 3, 4, 5];
 
 //Create new map
 let mapArray = myArray.map((value, index) => {
+    console.log(`index`, index);
     return value * 2;
 });
 console.log(mapArray);
